5105fe15142775d5de4b7f8b8cefc31fe09f1cd6
Alejandro Chanocua <73016752+alexchanocua@users.noreply.github.com>
2021-07-28 16:23:52 -0700

    changed App class to hook function

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React,  {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
@@ -6,47 +6,54 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
-
-    componentDidMount(){
+function App() {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+    // STATES
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response =>  response.json()) // conversting fetched data to JSON
+    //         .then(users => this.setState({robots:users})); // setting the robots array to the users
+    // }
+    // comparable to componentDidMount()
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response =>  response.json()) // conversting fetched data to JSON
-            .then(users => this.setState({robots:users})); // setting the robots array to the users
+            .then(users => {setRobots(users)}); // setting the robots array to the users
+    },[])
+    
+    const onSeacrhChange = (event) => {
+        setSearchfield(event.target.value);
     }
 
-    onSeacrhChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
 
-    render() {
-        const {robots, searchfield} = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
+    // if-else
+    return !robots.length ?
+            <h1>Loading</h1> :
+    (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={onSeacrhChange} />
+            <Scroll>
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
+                    
+            </Scroll>
+        </div>
+    );
 
-        // if-else
-        return !robots.length ?
-             <h1>Loading</h1> :
-        (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSeacrhChange} />
-                <Scroll>
-                    <ErrorBoundry>
-                        <CardList robots={filteredRobots}/>
-                    </ErrorBoundry>
-                     
-                </Scroll>
-            </div>
-        );
-    }
 
 }
 
